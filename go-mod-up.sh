5ffcf6b015df13d6ae9aa49aaa63e46c11734ad9
Marc Arndt <marndt@lemongrasscloud.com>
2022-07-21 11:55:59 +0200

    cleanup script

--- go-mod-up.sh
+++ go-mod-up.sh
@@ -1,12 +1,11 @@
-if ! go get -u 2>&1;
+if ! go get -u;
 then
     echo "Error: failed to execute go get -u"
     exit 1
 fi
 
-git diff --exit-code go.mod &> /dev/null
-
-if [ $? -eq 1 ]; then
+if ! git diff --exit-code go.mod;
+then
     echo "go.mod or go.sum differs, please re-add it to your commit"
     exit 1
 fi
